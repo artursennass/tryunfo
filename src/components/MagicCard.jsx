dece2518e8d87f4fc29286bf3d2051365dd836cb
Artur Senna <artursennass@gmail.com>
2022-07-27 16:23:36 -0300

    problemas de lint resolvidos

--- src/components/MagicCard.jsx
+++ src/components/MagicCard.jsx
@@ -1,114 +1,110 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
-export default class MagicCard extends Component {
-  tranformCardRare = (string) => {
+// export default class MagicCard extends Component {
+//   render() {
+//     const {
+//       cardName,
+//       cardDescription,
+//       // cardAttr1,
+//       // cardAttr2,
+//       // cardAttr3,
+//       cardImage,
+//       cardRare,
+//       // cardTrunfo,
+//     } = this.props;
 
-  };
+//     return (
+//       <div className="card-container">
 
-  render() {
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-    } = this.props;
+//         <div className="card-background" id={ cardRare }>
 
-    return (
-      <div className="card-container">
+//           <div className="card-frame">
 
-        <div className="card-background" id={ cardRare }>
+//             <div className="frame-header">
+//               <h1 data-testid="name-card" className="name">{ cardName }</h1>
+//             </div>
 
-          <div className="card-frame">
+//             <div className="centerImage">
+//               <img
+//                 src={ cardImage }
+//                 alt={ cardName }
+//                 data-testid="image-card"
+//                 className="frame-art"
+//               />
+//             </div>
 
-            <div className="frame-header">
-              <h1 data-testid="name-card" className="name">{ cardName }</h1>
-            </div>
+//             <div className="frame-type-line">
+//               <h1 data-testid="rare-card" className="type">{ cardRare.toUpperCase() }</h1>
+//               {/* {cardTrunfo
+//               && <h1
+//                 data-testid="trunfo-card"
+//                 className="type superTrunfo"
+//               >
+//                 Super Trunfo
 
-            <div className="centerImage">
-              <img
-                src={ cardImage }
-                alt={ cardName }
-                data-testid="image-card"
-                className="frame-art"
-              />
-            </div>
+//                  </h1>} */}
+//             </div>
 
-            <div className="frame-type-line">
-              <h1 data-testid="rare-card" className="type">{ cardRare.toUpperCase() }</h1>
-              {cardTrunfo
-              && <h1
-                data-testid="trunfo-card"
-                className="type superTrunfo"
-              >
-                Super Trunfo
+//             <div className="frame-text-box">
+//               <p
+//                 className="description"
+//                 data-testid="description-card"
+//               >
+//                 { cardDescription }
 
-                 </h1>}
-            </div>
+//               </p>
 
-            <div className="frame-text-box">
-              <p
-                className="description"
-                data-testid="description-card"
-              >
-                { cardDescription }
+//               <p
+//                 className="flavour-text"
+//               >
+//                 Para a todos conquistar.
+//               </p>
 
-              </p>
+//             </div>
 
-              <p
-                className="flavour-text"
-              >
-                Para a todos conquistar.
-              </p>
+//             {/* <div className="frame-bottom-info inner-margin">
+//               <div className="fbi-left">
+//                 <p>Projeto Tryunfo</p>
+//               </div>
 
-            </div>
+//               <div className="fbi-center">
+//                 Mana
+//                 <br />
+//                 { cardAttr1 }
+//               </div>
+//               <div className="fbi-center">
+//                 Atk
+//                 <br />
+//                 { cardAttr2 }
+//               </div>
+//               <div className="fbi-center">
+//                 Def
+//                 <br />
+//                 { cardAttr3 }
+//               </div>
 
-            <div className="frame-bottom-info inner-margin">
-              <div className="fbi-left">
-                <p>Projeto Tryunfo</p>
-              </div>
+//               <div className="fbi-right">
+//                 Produzido por Artur Senna
+//               </div>
+//             </div> */}
 
-              <div className="fbi-center">
-                Mana
-                <br />
-                { cardAttr1 }
-              </div>
-              <div className="fbi-center">
-                Atk
-                <br />
-                { cardAttr2 }
-              </div>
-              <div className="fbi-center">
-                Def
-                <br />
-                { cardAttr3 }
-              </div>
+//           </div>
 
-              <div className="fbi-right">
-                Produzido por Artur Senna
-              </div>
-            </div>
+//         </div>
 
-          </div>
+//       </div>
+//     );
+//   }
+// }
 
-        </div>
-
-      </div>
-    );
-  }
-}
-
-MagicCard.propTypes = {
-  cardName: PropTypes.string.isRequired,
-  cardDescription: PropTypes.string.isRequired,
-  cardAttr1: PropTypes.string.isRequired,
-  cardAttr2: PropTypes.string.isRequired,
-  cardAttr3: PropTypes.string.isRequired,
-  cardImage: PropTypes.string.isRequired,
-  cardRare: PropTypes.string.isRequired,
-  cardTrunfo: PropTypes.bool.isRequired,
-};
+// MagicCard.propTypes = {
+//   cardName: PropTypes.string.isRequired,
+//   cardDescription: PropTypes.string.isRequired,
+//   // cardAttr1: PropTypes.string.isRequired,
+//   // cardAttr2: PropTypes.string.isRequired,
+//   // cardAttr3: PropTypes.string.isRequired,
+//   cardImage: PropTypes.string.isRequired,
+//   cardRare: PropTypes.string.isRequired,
+//   // cardTrunfo: PropTypes.bool.isRequired,
+// };
